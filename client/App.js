5556eb300a9b30c6980a467cf42f3028f75fb19c
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-03-30 15:45:12 -0300

    Menu Header

--- client/App.js
+++ client/App.js
@@ -1,22 +1,24 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { Provider } from "react-native-paper";
 import Header from "./src/components/Header";
 import SignUp from "./src/pages/SignUp";
 
 export default function App() {
-	return (
-		<View>
-			<Header type="chat" />
-			<View style={styles.container}></View>
-		</View>
-	);
+  return (
+    <Provider>
+      <Header type="chat" />
+      <View style={styles.container}></View>
+    </Provider>
+  );
 }
 
 const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
+  container: {
+    flex: 1,
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
+  },
 });
