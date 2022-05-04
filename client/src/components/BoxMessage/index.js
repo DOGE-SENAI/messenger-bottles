21bb99cb9bd6c675a7f31e81fc61467cca1ce738
CauaRodrigues <caua.rodriguesassis205@gmail.com>
2022-05-04 17:18:28 -0300

    Dialog

--- client/src/components/BoxMessage/index.js
+++ client/src/components/BoxMessage/index.js
@@ -1,94 +1,99 @@
-import { StyleSheet, Text, View, TouchableOpacity } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Pressable,
+  TouchableOpacity,
+} from "react-native";
 import { Avatar } from "react-native-paper";
 
 const Man2 = "https://imgur.com/qOemCDS.png";
 
-export default function BoxMessages({ from, sender, message }) {
-	const username1 = "Yun Li";
-	const username2 = "Biffe";
+export default function BoxMessages({ from, sender, message, onLongPress }) {
+  return (
+    <View style={styles.containerContact(sender)}>
+      <View style={styles.contentMessage}>
+        <View style={styles.userMessage(sender)}>
+          {sender && (
+            <Avatar.Image
+              size={50}
+              style={styles.avatar}
+              source={{ uri: Man2 }}
+            />
+          )}
 
-	return (
-		<View style={styles.containerContact(sender)}>
-			<View style={styles.contentMessage}>
-				{sender && (
-					<View style={styles.userMessage2}>
-						<Avatar.Image
-							size={50}
-							style={styles.avatar}
-							source={{ uri: Man2 }}
-						/>
+          <Text style={styles.users}>{from}</Text>
 
-						<Text style={styles.users}>{username2}</Text>
-					</View>
-				)}
+          {!sender && (
+            <Avatar.Image
+              size={50}
+              style={styles.avatar}
+              source={{ uri: Man2 }}
+            />
+          )}
+        </View>
 
-				{!sender && (
-					<View style={styles.userMessage1}>
-						<Text style={styles.users}>{username1}</Text>
-
-						<Avatar.Image
-							size={50}
-							style={[styles.avatar, styles.shadowProp]}
-							source={{ uri: Man2 }}
-						/>
-					</View>
-				)}
-				<Text
-					style={[styles.message, sender ? styles.recipient : styles.sender]}
-				>
-					{message}
-				</Text>
-			</View>
-		</View>
-	);
+        <TouchableOpacity onLongPress={() => onLongPress(true, from)}>
+          <Text
+            style={[styles.message, sender ? styles.recipient : styles.sender]}
+          >
+            {message}
+          </Text>
+        </TouchableOpacity>
+      </View>
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
-	containerContact(directionMessage) {
-		return {
-			flexDirection: "row",
-			borderRadius: 20,
-			margin: 8,
-			width: "100%",
-			justifyContent: directionMessage ? "flex-start" : "flex-end",
-		};
-	},
-	contentMessage: {
-		flexDirection: "column",
-	},
-	userMessage2: {
-		flexDirection: "row",
-		alignItems: "flex-end",
-		justifyContent: "flex-start",
-	},
-	userMessage1: {
-		flexDirection: "row",
-		alignItems: "flex-end",
-		justifyContent: "flex-end",
-	},
-	users: {
-		fontSize: 16,
-		margin: 4,
-		marginRight: 6,
-	},
-	message: {
-		fontSize: 20,
-		textAlign: "left",
-		alignSelf: "center",
-		padding: 16,
-		borderRadius: 16,
-	},
-	sender: {
-		backgroundColor: "#7DBDED",
-		color: "#000000",
-	},
-	recipient: {
-		backgroundColor: "#000000",
-		color: "#ffffff",
-	},
-	avatar: {
-		backgroundColor: "white",
-		bottom: -14,
-		zIndex: 1,
-	},
+  containerContact(directionMessage) {
+    return {
+      flexDirection: "row",
+      borderRadius: 20,
+      margin: 8,
+      width: "100%",
+      justifyContent: directionMessage ? "flex-start" : "flex-end",
+    };
+  },
+  contentMessage: {
+    flexDirection: "column",
+  },
+  userMessage(sender) {
+    return sender
+      ? {
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "flex-start",
+        }
+      : {
+          flexDirection: "row",
+          alignItems: "flex-end",
+          justifyContent: "flex-end",
+        };
+  },
+  users: {
+    fontSize: 16,
+    margin: 4,
+    marginRight: 6,
+  },
+  message: {
+    fontSize: 20,
+    textAlign: "left",
+    alignSelf: "center",
+    padding: 16,
+    borderRadius: 16,
+  },
+  sender: {
+    backgroundColor: "#7DBDED",
+    color: "#000000",
+  },
+  recipient: {
+    backgroundColor: "#000000",
+    color: "#ffffff",
+  },
+  avatar: {
+    backgroundColor: "white",
+    bottom: -14,
+    zIndex: 1,
+  },
 });
